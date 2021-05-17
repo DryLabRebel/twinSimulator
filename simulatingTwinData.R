e1be11fc716e588ebdbf98fd16ac05a06cac7a8a
Geoff English <geoffrey.english@uqconnect.edu.au>
2021-05-17 11:06:36 +1000

    more fiddling around. Added a zygosity column

--- simulatingTwinData.R
+++ simulatingTwinData.R
@@ -1,11 +1,15 @@
 #!/usr/bin/env Rscript
 
+# Accept command line arguments
 args <- commandArgs( trailingOnly = TRUE )
 
+# Documentation
 print("Correlated twin data simulator. Accepts three inputs, must be in order: MZr (MZ correlation), DZr (DZ correlation), N (number of twin pairs).", quote=F)
 
+# store current output warnings setting
 oldw <- getOption("warn")
 
+# Suppress warnings: will throw a warning if a non-numeric is entered - but I have my own warnings below, which are clearer which are more user friendly
 options(warn = -1)
 if ( is.na( args[1] ) == T ) {
   print( "No MZ correlation detected. Default ( 0.8 ) will be used.", quote = F )
@@ -27,10 +31,10 @@ if ( is.na( args[3] ) == T ) {
 } else {
   N <<- as.numeric( args[3] )
 }
+# reactivate warnings
 options(warn = oldw)
 
-# Checking for bad input
-
+# Checking for bad/non-numeric input
 if ( is.na(MZr) == T ) {
   stop( 
        "Bad input. Your MZ correlation is not a numeric value", call. = FALSE
@@ -45,7 +49,7 @@ if ( is.na(MZr) == T ) {
   )
 } 
 
-# sanity test for MZ correlation
+# sanity test for input
 if ( MZr < ( -1 ) | MZr > 1 ) {
   stop( 
        "Bad input. Your MZ correlation is out of bounds. 
@@ -66,7 +70,7 @@ if ( MZr < ( -1 ) | MZr > 1 ) {
   )
 } else if ( MZr < DZr ) {
 # Warning when MZ correlation is less than DZ correlation - might happen if user inputs correlation in the wrong order
-  print( "supplied DZ correlation is larger than MZ correlation, this is unusual ( but not impossible ). Please ensure you entered your arguments in order: MZr, DZr, N.", quote = F )
+  print( "WARNING: supplied DZ correlation is larger than MZ correlation, this is unusual ( but not impossible ). Was this intentional? Please ensure you entered your arguments in order: MZr, DZr, N.", quote = F )
 }
 
 library( MASS )
@@ -106,13 +110,29 @@ names( mz )[names( mz ) == "V2"] <- "twin2"
 names( dz )[names( dz ) == "V1"] <- "twin1"
 names( dz )[names( dz ) == "V2"] <- "twin2"
 
-head( dz )
+mz$zyg <- "mz"
+dz$zyg <- "dz"
+
 head( mz )
+head( dz )
 
 # tidy up the above
-# give your columns names
+# *Check* (17-05-2021) - # give your columns names
+# *Check* (17-05-2021) - # add in some conditions - default values, checks for bad input, etc.
+
+# find a way to uniquely specify arguments, so you can leave MZ blank, but still input DZr and/or N, so MZ uses default
+
 # add in age and sex variables
-# add in some conditions - default values, checks for bad input, etc.
+
+# add in zyg ID
+
+# add in FID and IID
+
+# rbind into 1 fam per line
+
+# save output as a *.txt file - use 'args' as filename - Twimsim_MZr[MZr]_DZr[DZr]_N[N].txt
+
+# when printing, suppress the '[1]' at the beginning of the line - i.e just print the output, as output
 
 # Don't take forever - get something that works, and shows off a few things
 # What next?
