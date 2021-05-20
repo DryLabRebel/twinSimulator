7178707a8edb99395a660707d3c8d64c9cf4bba7
Geoff English <geoffrey.english@uqconnect.edu.au>
2021-05-21 09:35:41 +1000

    Created sex columns, assigned a zyg ID, based on zygosity status, and sex. Includes dz birth order

--- simulatingTwinData.R
+++ simulatingTwinData.R
@@ -6,7 +6,13 @@ args <- commandArgs(trailingOnly = TRUE, asValues = T, args = c("mzr", "dzr", "N
 
 # Documentation
 cat("\nCorrelated twin data simulator.\nAccepts three inputs, --mzr (MZ correlation), --dzr (DZ correlation), --N (number of twin pairs).\n\n")
-
+cat("Twins are randomly assigned a gender and zygosity status, \"1\" for males and \"0\" for females.\n\n")
+cat("'mzm' = monozygotic males\n")
+cat("'mzf' = monozygotic females\n")
+cat("'dzm' = dizygotic male pair\n")
+cat("'dzf' = dizygotic female pair\n")
+cat("'dzM' = dizygotic twin pair, first born is male\n")
+cat("'dzF' = dizygotic twin pair, first born is female\n\n")
 # store current output warnings setting
 oldw <- getOption("warn")
 
@@ -70,8 +76,12 @@ if (mzr < (-1) | mzr > 1) {
        and/or please ensure you entered your arguments in order: mzr, dzr, N.\n\n", call. = FALSE
  )
 } else if (mzr < dzr) {
-# Warning when MZ correlation is less than DZ correlation - might happen if user inputs correlation in the wrong order
-  cat("WARNING: supplied DZ correlation is larger than MZ correlation, this is unusual (but not impossible). Was this intentional? Please ensure you entered your arguments in order: mzr, dzr, N.\n\n")
+# Warnings regarding unusual or unlikely twin pair correlations
+  cat("WARNING: supplied DZ correlation is larger than MZ correlation, this is unusual (but not impossible). Was this intentional?\n\n")
+} else if (mzr < 0) {
+  cat("WARNING: supplied MZ correlation is less than zero, in real world data this is extremely unlikely (but not impossible). Was this intentional?\n\n")
+} else if (dzr < 0) {
+  cat("WARNING: supplied DZ correlation is less than zero, in real world data this is unusual (but not impossible). Was this intentional?\n\n")
 }
 
 library(MASS, quietly = T)
@@ -105,14 +115,45 @@ DZmu <- c(11, 11)
 mz <- as.data.frame(mvrnorm(n = N, mu = MZmu, Sigma = mzCov))
 dz <- as.data.frame(mvrnorm(n = N, mu = DZmu, Sigma = dzCov))
 
+# Super informative Stack post - https://stats.stackexchange.com/questions/336166/what-is-the-difference-between-dbinom-and-dnorm-in-r
+
+sexmz <- rbinom(n = N, 1, 0.5)
+sexd1 <- rbinom(n = N, 1, 0.5)
+sexd2 <- rbinom(n = N, 1, 0.5)
+
+mz$sex1 <- sexmz
+mz$sex2 <- mz$sex1
+dz$sex1 <- sexd1
+dz$sex2 <- sexd2
+
+mz$zyg[mz$sex1 == 1]  <- "mzm"
+mz$zyg[mz$sex1 == 0]  <- "mzf"
+
+dz$zyg[
+       dz$sex1 == 1 &
+       dz$sex2 == 1
+       ]  <- "dzm"
+
+dz$zyg[
+       dz$sex1 == 0 &
+       dz$sex2 == 0
+       ]  <- "dzf"
+
+dz$zyg[
+       dz$sex1 == 1 &
+       dz$sex2 == 0
+       ]  <- "dzM"
+
+dz$zyg[
+       dz$sex1 == 0 &
+       dz$sex2 == 1
+       ]  <- "dzF"
+
 names(mz)[names(mz) == "V1"] <- "twin1"
 names(mz)[names(mz) == "V2"] <- "twin2"
 
 names(dz)[names(dz) == "V1"] <- "twin1"
 names(dz)[names(dz) == "V2"] <- "twin2"
-
-mz$zyg <- "mz"
-dz$zyg <- "dz"
 
 head(mz)
 head(dz)
@@ -128,7 +169,31 @@ head(dz)
 
 # add in age and sex variables
 
-# add in zyg ID
+# Hang on. Sex is not quite so simple. I need to assign a sex value to each individual - and then need a way to determine zygosity
+# Ah, no, only for dz. For MZ the sex will be the same in every case
+
+# So what do I need to do?
+# I need a zygosity column - "01" mzf, "02" mzm, "03" dzf, "04" dzm, "05" dzo
+
+# I need to create a sex column for each twin
+
+# for mzs, I need the same value in each column
+# for dzs, I can randomly assign the values. So... if I just randomly assign 0's and 1's to each twin... it will be 50% dzo. Yep. Which is probably biologically realistic... is it?
+
+# So, the simplest thing would be to just duplicate the mz sex column, and run two separte dz columns
+
+# What do I want to do?
+# Create an age variable for my twin pairs
+# Do I want to just randomly assign an age (with a mean and sd) for each row of my dataset?
+  # Well, if I want there to be an age dependent effect then maybe I'd want it to be correlated with my twin variables?
+  # I could specify an optional age effect parameter - so the correlated variables would be randomly generated, but weighted by age?
+  # Lets just start simple...
+    # Generate a random vector of age variables of length N ( or dob? ) - so age could be dependent on some future variable (date of data collection!)
+# When you run a twin study - what kinds of things are common?
+  # You're looking for a miminum age?
+  # You're looking for an age range?
+
+# *Check* (21-05-2021) - # add in zyg ID
 
 # add in FID and IID
 
